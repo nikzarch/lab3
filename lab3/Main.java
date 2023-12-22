c6697a7f0b995e3737506f25ca6ec5d718d0f6da
nikzarch <153365178+nikzarch@users.noreply.github.com>
2023-12-23 00:02:44 +0300

    Create Main.java

---
+++ lab3/Main.java
@@ -0,0 +1,30 @@
+import characters.Julius;
+import characters.Kid;
+import environment.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Julius julius = new Julius();
+        Kid kid = new Kid();
+        Environment bedroom = new Environment("Спальня");
+        Container glass = new Container("Стакан с водой", bedroom);
+        Thing jaws = new Thing("Челюсти", bedroom);
+        Thing glasses = new Thing("Очки", bedroom);
+        Candy[] bag = {Candy.ROTFRONT, Candy.CHUPACHUPS, Candy.KREMKA};
+        CandyBag candyBag = new CandyBag("Кулёк с карамельками", bag, bedroom);
+
+        bedroom.sound("грр-пс-пс");
+        bedroom.changeTime(DayTime.MORNING);
+        julius.sleep();
+        glass.lay();
+        kid.see(glass);
+        kid.take(jaws);
+        kid.put(glass);
+        glasses.lay();
+        candyBag.lay();
+        kid.see(candyBag);
+        candyBag.printContent();
+        kid.take(candyBag, "чтобы отдать Карлсону");
+        kid.take(candyBag.takeCandy(0)); // карман будет занят
+    }
+}
